8a1a74869f40a1d65ca2955e3aeb9405434f4606
Shaikat0079 <baruashaikat00790@gmail.com>
2025-02-14 02:13:34 +0930

    Two tasks completed

---
+++ index.js
@@ -0,0 +1,19 @@
+const colors = ['red', 'blue', 'green', 'yellow', 'orange']
+const newColors = []
+for(let i=colors.length-1;i>=0;i--){
+    // console.log(colors[i])
+    newColors.push(colors[i])
+}
+console.log(newColors)
+
+const numbers = [12, 98, 5, 41, 23, 78, 46];
+
+const even_numbers = [];
+
+for (let i = 0; i<numbers.length;i++){
+    if(numbers[i]%2===0){
+        even_numbers.push(numbers[i]);
+    }
+}
+
+console.log(even_numbers)
